a168dcde69ab1cc68865c9f5fe28eb7eb49e638d
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2022-03-18 09:33:55 -0400

    Adding Behavior & Models

---
+++ XFMapsSample/Models/LabourZone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace XFMapsSample.Models
+{
+    //public class LabourZone: List<Point>
+    //{
+    //    public List<Point> Points => this;
+
+    //    public LabourZone(IEnumerable<Point> items)
+    //    {
+    //        AddRange(items);
+    //    }
+    //    
+    //}
+
+    //Podría ser que la zona laboral herede de una Lista de poligonos
+    public class LabourZone: List<Polygon>
+    {
+        public List<Polygon> Polygons => this;
+
+        public LabourZone(IEnumerable<Polygon> items)
+        {
+            AddRange(items);
+        }
+    }
+
+    
+}
